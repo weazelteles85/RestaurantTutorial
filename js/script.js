eb4d2ef4766b6864d35cc4027c47b946f4018d49
weazelteles85 <claudioteles85@gmail.com>
2016-11-18 12:08:57 -0500

    update all

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(function () {
+	$("#navbar-toggle collapsed").blur(function (event) {
+		var screenWidth = window.innerWidth;
+		if (screenWidth < 768) {
+			$("#collapsable-nav").collapse('hide');
+		}
+	});
+});
+
+(function (global) {
+	var dc = {};
+
+	var homeHtml = "snippets/home-snippet.html";
+
+	// Convinience function for inserting innerHTML for 'select'
+	var insertHtml = function (selector, html) {
+		var targetElem = document.querySelector(selector);
+		targetElem.innerHTML = html;
+	};
+
+	// Show loading icon inside element identified by 'selector'.
+	var showLoading = function (selector) {
+		var html = "<div class='text-center'>";
+		html += "<img src='fonts/Images/ajax-loader.gif'></div>";
+		insertHtml(selector, html);
+	};
+
+	// On page load (before images or CSS)
+	document.addEventListener("DOMContentLoaded", function (event) {
+
+		// On first load, show home view
+		showLoading("#main-content");
+		$ajaxUtils.sendGetRequest (homeHtml, function (responseText) {
+			document.querySelector("#main-content").innerHTML = responseText;
+		}, false);
+	});
+
+	global.$dc = dc;
+
+})(window);
