b09ebe1ec9108e311aa322f18d33bd84fd775a35
weazelteles85 <claudioteles85@gmail.com>
2016-11-29 12:18:49 -0500

    Update

--- js/script.js
+++ js/script.js
@@ -11,6 +11,9 @@ $(function () {
 	var dc = {};
 
 	var homeHtml = "snippets/home-snippet.html";
+	var allCategoriesUrl = "http://davids-restaurant.herokuapp.com/categories.json";
+	var categoriesTitleHtml = "snippets/categories-title-snippet.html";
+	var categoryHtml = "snippets/ategory-snippet.html";
 
 	// Convinience function for inserting innerHTML for 'select'
 	var insertHtml = function (selector, html) {
@@ -25,6 +28,14 @@ $(function () {
 		insertHtml(selector, html);
 	};
 
+	// Return substitute of '{{propName}}'
+	// with propValue in given 'string'
+	var insertProperty = function (string, propName, propValue) {
+		var propToReplace = "{{" + propName + "}}";
+		string = string.replace(new RegExp(propToReplace, "g"), propValue);
+		return string;
+	}
+
 	// On page load (before images or CSS)
 	document.addEventListener("DOMContentLoaded", function (event) {
 
@@ -34,6 +45,49 @@ $(function () {
 			document.querySelector("#main-content").innerHTML = responseText;
 		}, false);
 	});
+
+	// Load the menu categories view
+	dc.loadMenuCategories = function () {
+		showLoading("#main-content");
+		$ajaxUtils.sendGetRequest(allCategoriesUrl, buildAndShowCategoriesHTML);
+	};
+
+	//Builds HTML for the categories page based on the data from the server
+	function buildAndShowCategoriesHTML (categories) {
+		//Load title snippet of categories page
+		$ajaxUtils.sendGetRequest(categoriesTitleHtml, function (categoriesTitleHtml) {
+			//retrieve single category snippet
+			$ajaxUtils.sendGetRequest(categoryHtml, function (categoryHtml) {
+				var categoriesViewHtml = buildCategoriesViewHtml(categories, categoriesTitleHtml, categoryHtml);
+				insertHtml("#main-content", categoriesViewHtml);
+			}, false);
+		},
+		false);
+	}
+
+	// Using categories data and snippets html
+	// build categories view HTML to be inserted into page
+	function buildCategoriesViewHtml(categories, categoriesTitleHtml, categoryHtml) {
+		var finalHtml = categoriesTitleHtml;
+		finalHtml += "<section class='row'>";
+
+		//Loop over categories
+		for (var i = 0; i < categories.length; i++) {
+			//insert category values
+			var html = categoryHtml;
+			var name = "" + categories[i].name;
+			var short_name = categories[i].short_name;
+			html = insertProperty(html, "name", name);
+			html = insertProperty(html, "short_name", short_name);
+			finalHtml += html;
+		}
+
+		finalHtml += "</section>";
+		return finalHtml;
+	}
+
+
+
 
 	global.$dc = dc;
 
