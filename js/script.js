9928300118bc210247a2224799588b99f2b07dcc
weazelteles85 <claudioteles85@gmail.com>
2016-11-29 15:00:57 -0500

    Hopefully pics

--- js/script.js
+++ js/script.js
@@ -15,6 +15,9 @@ $(function () {
 	var allCategoriesUrl = "dataSource.json";
 	var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 	var categoryHtml = "snippets/category-snippet.html";
+	var menuItemsUrl = "detailedDataSource.json?category=";
+	var menuItemsTitleHtml = "snippets/menu-items-title.html";
+	var menuItemHtml = "snippets/menu-item.html";
 
 	// Convinience function for inserting innerHTML for 'select'
 	var insertHtml = function (selector, html) {
@@ -53,6 +56,12 @@ $(function () {
 		$ajaxUtils.sendGetRequest(allCategoriesUrl, buildAndShowCategoriesHTML);
 	};
 
+	// Load the menu items view 'categoryShort' is a short_name for a category
+	dc.loadMenuItems = function (categoryShort) {
+		showLoading("#main-content");
+		$ajaxUtils.sendGetRequest(menuItemsUrl + categoryShort, buildAndShowMenuItemsHTML);
+	};
+
 	//Builds HTML for the categories page based on the data from the server
 	function buildAndShowCategoriesHTML (categories) {
 		//Load title snippet of categories page
@@ -86,6 +95,84 @@ $(function () {
 		finalHtml += "</section>";
 		return finalHtml;
 	}
+
+	//Builds HTML for the single category page based on the data from the server
+	function buildAndShowMenuItemsHTML (categoryMenuItems) {
+		//Load title snippet of menu items page
+		$ajaxUtils.sendGetRequest(menuItemsTitleHtml. function (menuItemsTitleHtml) {
+			//Retrieve single menu item snippet
+			$ajaxUtils.sendGetRequest(menuItemHtml, function (menuItemHtml) {
+				var menuItemsViewHtml = buildMenuItemsViewHtml(categoryMenuItems, menuItemsTitleHtml, menuItemHtml);
+				insertHtml("#main-content", menuItemsViewHtml);
+			}, false);
+		},
+		false);
+	}
+
+	//using category and menu items data and snippets html
+	// build menu items HTML to be inserted into page
+	function buildMenuItemsViewHtml (categoryMenuItems, menuItemsTitleHtml, menuItemHtml) {
+		menuItemsTitleHtml = insertProperty(menuItemsTitleHtml, "name", categoryMenuItems.category.name);
+		menuItemsTitleHtml = insertProperty(menuItemsTitleHtml, "special_instructions", categoryMenuItems.category.special_instructions);
+
+		var finalHtml = menuItemsTitleHtml;
+		finalHtml += "<section class='row'>";
+
+		//Loop over menu items
+		var menuItems = categoryMenuItems.menu_items;
+		var catShortName = categoryMenuItems.category.short_name;
+
+		for (var i = 0; i < menuItems.length; i++) {
+			//insert menu items values
+			var html = menuItemHtml;
+			html = insertProperty(html, "short_name", menuItems[i].short_name);
+			html = insertProperty(html, "catShortName", catShortName);
+			html = insertItemPrice(html, "price_small", menuItems[i].price_small);
+			html = insertItemPortionName(html, "small_portion_name", menuItems[i].small_portion_name);
+			html = insertItemPrice(html, "price_large", menuItems[i].price_large);
+			html = insertItemPortionName(html, "large_portion_name", menuItems[i].large_portion_name);
+			html = insertProperty(html, "name", menuItems[i].name);
+			html = insertProperty(html, "description", menuItems[i].description);
+
+			// add clearfix after every second menu item
+			if (i % 2 != 0) {
+				html += "<div class='clearfix xisible-lg-block visible-md-block'></div>";
+			}
+
+			finalHtml += html;
+		}
+
+		finalHtml += "</section>";
+		return finalHtml;
+	}
+
+	// Appends price with '$' if price exists
+	function insertItemPrice(html, pricePropName, priceValue) {
+		// If not specified, replace with empty string
+		if (!priceValue) {
+			return insertProperty(html, pricePropName, "");
+		}
+
+		priceValue = "$" + priceValue.toFixed(2);
+		html = insertProperty(html, pricePropName, priceValue);
+		return html;
+	}
+
+	// Appends portion name in parens if it exists
+	function insertItemPortionName(html, portionPropName, portionValue) {
+		// if not specified, return original string
+		if (!portionValue) {
+			return insertProperty(html, portionPropName, "");
+		}
+
+		else {
+			html = insertProperty(html, portionPropName, portionValue);
+			return html;
+		}
+
+	}
+
+
 
 
 
